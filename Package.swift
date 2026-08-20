1a09d1ffb9023a3c5a5603e0b058b75b043c2812
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:52:07 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -21,10 +21,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-iso-15924",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(name: "ISO 15924", targets: ["ISO 15924"])
